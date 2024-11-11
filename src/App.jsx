a8b7b0a93d29621c789b93c405fc751ccd2f2bba
kalebemax <169088614+kalebemax@users.noreply.github.com>
2024-11-11 16:29:45 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState, useRef } from 'react'
+import axios from 'axios'
+import './App.css'
+import WeatherInformations from './components/WeatherInformations/WeatherInformations'
+import WeatherInformations5Days from './components/WeatherInformations5Days/WeatherInformations5Days'
+
+function App() {
+  const [weather, setweather] = useState()
+  const [weather5Days, setweather5Days] = useState()
+  const inputRef = useRef()
+
+  async function searchCity() {
+    const city = inputRef.current.value
+    const key = "d368b957cb7f7e01439da980d7543fae"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+    const url5Days = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&lang=pt_br&units=metric`
+    const apiInfo = await axios.get(url)
+    const apiInfo5Days = await axios.get(url5Days)
+    setweather5Days(apiInfo5Days.data)
+    setweather(apiInfo.data)
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      searchCity()
+    }
+  }
+
+  return (
+    <div className='container'>
+      <h1>DevClub Previsão do Tempo</h1>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder='Digite o nome da cidade'
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={searchCity}>Buscar</button>
+
+      {weather && <WeatherInformations weather={weather} />}
+      {weather5Days && <WeatherInformations5Days  weather5Days={weather5Days}/>}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WeatherInformations/WeatherInformations.jsx
@@ -0,0 +1,26 @@
+import './WeatherInformations.css'
+
+function WeatherInformations({ weather }) {
+  console.log(weather);
+
+  return (
+    <div className='weather-container'>
+        <h2>{weather.name}</h2>
+        <div className='weather-info'>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+            alt="Weather Icon"
+          />
+          <p className='temperature'>{Math.round(weather.main.temp)}°C</p>
+        </div>
+        <p className='description'>{weather.weather[0].description}</p>
+        <div className='details'>
+          <p>Sensação Térmica: {Math.round(weather.main.feels_like)}°C</p>
+          <p className='humidity'>Umidade: {Math.round(weather.main.humidity)}%</p>
+          <p>Pressão: {Math.round(weather.main.pressure)} hPa</p>
+        </div>
+    </div>
+  );
+}
+
+export default WeatherInformations;
